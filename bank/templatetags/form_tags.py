0a99e51b8e33bfa4bcccc77860def7cc23d59a95
Diego Murillo <diegoa31052004@gmail.com>
2025-03-29 22:56:33 -0500

    feat: All styles ready

--- bank/templatetags/form_tags.py
+++ bank/templatetags/form_tags.py
@@ -1,6 +1,6 @@
 from django import template
 register = template.Library()
 
-@register.filter(name='addclass')
-def addclass(field, css):
+@register.filter(name='add_class')
+def add_class(field, css):
     return field.as_widget(attrs={"class": css})
